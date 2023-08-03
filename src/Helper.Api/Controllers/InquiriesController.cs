b57ba8b6f81867735522f3c20c266a7577cefe9e
Dmytro Protsailo <dmytro.protsailo@billennium.com>
2023-08-03 12:06:40 +0200

    Added basic functionality controllers skelets

---
+++ src/Helper.Api/Controllers/InquiriesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Helper.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InquiriesController : ControllerBase
+    {
+        //private readonly ICommandHandler<InquiriStatus> _changeInquiriStatusHandler;
+        //private readonly ICommandHandler<CreateInquiri> _createInquiriHandler;
+        //private readonly ICommandHandler<FeasibilityNote> _setFeasibilityHandler;
+
+        [HttpPost(""), AllowAnonymous]
+        public async Task<ActionResult> CreateInquiri()//CreateInquiri command)
+        {
+            //await _createInquiriHandler.HandleAsync(command);
+            return Ok();
+        }
+
+        [HttpPut("feasibility-note/{inquiryId}"), Authorize]
+        public async Task<ActionResult> SetFeasibility()//int inquiryId, FeasibilityNote command)
+        {
+            //await _setFeasibilityHandler.HandleAsync(command with { inquiriId = inquiryId });
+            return Ok();
+        }
+
+
+        [HttpPut("accepted/{inquiryId}")]
+        public async Task<ActionResult> AcceptInquiri(int inquiryId)
+        {
+            //await _changeInquiriStatusHandler.HandleAsync(new InquiriStatus(inquiryId, true));
+            return Ok();
+        }
+
+        [HttpPut("rejected/{inquiryId}")]
+        public async Task<ActionResult> RejectInquiri(int inquiryId)
+        {
+            //await _changeInquiriStatusHandler.HandleAsync(new InquiriStatus(inquiryId, false)); ;
+            return Ok();
+        }
+
+        [HttpGet(""), Authorize]
+        public async Task<ActionResult> GetInquiries()
+        {
+            return Ok();// await _getInquiries.GetAll());
+        }
+
+        [HttpGet("{inquiriId}"), Authorize]
+        public async Task<ActionResult> GetInquiri()
+        {
+            return Ok();// await _getInquiries.GetInquiri(inquiriId));
+        }
+
+        [HttpGet("solutions-variants"), Authorize]
+        public async Task<ActionResult> GetInquiriSolutionVariants(int inquiriId)
+        {
+            return Ok();// await _getInquiries.GetInquiriSolutionVariants(inquiriId));
+        }
+    }
+}
---
+++ src/Helper.Api/Controllers/SecurityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Helper.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecurityController : ControllerBase
+    {
+        [HttpPost("authorize")]
+        public async Task<ActionResult> AuthorizeUser()
+        {
+            return Ok();//await _authorization.authorize());
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser()
+        {
+            return Ok();// await _registration.register());
+        }
+    }
+}
