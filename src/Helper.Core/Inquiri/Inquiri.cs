8b5e24fd5cee6b224bce2403ffcd114d67c534a8
Dmytro Protsailo <dmytro.protsailo@billennium.com>
2023-08-03 12:09:20 +0200

    Added basic entitties

---
+++ src/Helper.Core/Inquiri/Inquiri.cs
@@ -0,0 +1,16 @@
+﻿namespace Helper.Core.Entities
+{
+    internal class Inquiri
+    {
+        public Guid Id { get; private set; }
+        public string Description { get; private set; }
+        public DateOnly RequestedCompletionDate { get; private set; }
+        public string SolutionDecision { get; private set; } = "Awaits decision";
+
+        public Inquiri(string description, DateOnly requestedCompletionDate)
+        {
+            Description = description;
+            RequestedCompletionDate = requestedCompletionDate;
+        }
+    }
+}
